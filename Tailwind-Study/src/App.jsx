c8336822bb84ada704260e2316b743be1d06da0f
Day-Ham <daehankimm17@gmail.com>
2025-07-10 17:38:29 +0800

    Added Placeholder Lines

--- Tailwind-Study/src/App.jsx
+++ Tailwind-Study/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import ButtonGradient from "./assets/svg/ButtonGradient"
 import Button from './components/Button'
+import Header from './components/Header'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-       
+       <Header></Header>
         <h1 className='text-cyan-500 underline font-extrabold'>Hello Dae Han</h1>
         <div className='pt-[4.75rem] lg:pt-p[5.25rem] overflow-hidden'>
          <Button className="mt-10"  >
--- Tailwind-Study/src/components/Header.jsx
+++ Tailwind-Study/src/components/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {brainwave} from'../assets'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='fixed top-0 z-50 bg-neutral-700-800 backdrop-blur-sm border-b border-neutral-800'>
+        <div className='flex items-center px-5 lg:px-7.5 xl:px-10'>
+        <a className='block w-[12rem] xl:mr-8' href="#hero" >
+            <img src={brainwave} width={190} height={40} alt='Brainwave' />
+        </a>
+        <nav className='hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-neutral-50 lg:static lg:flex lg:mx-auto lg:bg-transparent'>
+            <div className='relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row'> 
+                {navigation}
+            </div>
+        </nav>
+        </div>
+
+
+    </div>
   )
 }
 
